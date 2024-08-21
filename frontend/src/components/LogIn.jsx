f2986074d224e341697b78256f02c876c8a2e123
ThaiHoang <s3988412@rmit.edu.vn>
2024-08-21 23:35:23 +0700

    Update LogIn.jsx

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -14,18 +14,23 @@ export default function LogIn({hideIcon, setHideIcon, pwdInputType, setPwdInputT
     return (
         <div>
             <div className="flex flex-col items-center justify-center h-screen w-full">
-                <div className="w-full max-w-md bg-[#222] rounded-xl shadow-md py-8 px-8">
+                <div
+                    className="w-full max-w-md bg-gradient-to-r from-sky-400 to-sky-200 rounded-xl shadow-md py-8 px-8">
                     <h2 className={'text-[28px] font-bold text-white mb-6 text-center'}>Log In</h2>
                     <form className={'flex flex-col'}>
                         <input placeholder={'Username'}
-                               className={'bg-gray-700 text-white border-0 rounded-md p-2 mb-4 focus:outline-none transition ease-in duration-150 placeholder-gray-300'}
-                               type='text'/>
+                               className={'bg-gray-100 text-black border-0 rounded-md p-2 mb-4 focus:outline-none transition ease-in duration-150 placeholder-gray-300'}
+                               name={'username'}
+                               type='text'
+                               onChange={event => updateAccountInfo(event)}/>
                         <div className={'flex space-x-4'}>
                             <input placeholder={'Password'}
-                                   className={'bg-gray-700 text-white border-0 rounded-md p-2 mb-4 w-11/12 focus:outline-none transition ease-in duration-150 placeholder-gray-300'}
-                                   type={`${pwdInputType}`}/>
+                                   className={'bg-gray-100 text-black border-0 rounded-md p-2 mb-4 w-11/12 focus:outline-none transition ease-in duration-150 placeholder-gray-300'}
+                                   name={'password'}
+                                   type={`${pwdInputType}`}
+                                   onChange={event => updateAccountInfo(event)}/>
                             <button onClick={() => handleOnClick()} type={'button'}>
-                                <span className={"material-icons place-content-center"}>{hideIcon}</span>
+                                <span className={"material-icons place-content-center text-gray-600"}>{hideIcon}</span>
                             </button>
                         </div>
                         <button
