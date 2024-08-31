f07815c0a80cedc5c5688b2a039823c352b37421
ThePham10 <s3981133@rmit.edu.vn>
2024-09-01 02:15:17 +0700

    add js files

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { getUserProfile } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const {registerUser, loginUser} = require("../controllers/authController");
+
+const router = express.Router();
+
+router.get('/profile', protect, getUserProfile);
+
+router.post('/register', registerUser)
+
+router.post('/login', loginUser)
+
+
+
+module.exports = router;
