9da4737c38644db45e77f41f0437627227217ece
Trong Nhan <92970779+NMTNhan@users.noreply.github.com>
2024-09-02 00:39:06 +0700

    Delete utils directory

--- utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '1d', // Set expiration time, e.g., 1 day
-  });
-};
-
-module.exports = generateToken;
