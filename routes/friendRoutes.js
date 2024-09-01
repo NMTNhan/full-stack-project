1818cf4e08cfd03ae024b8338748851c85081572
Trong Nhan <92970779+NMTNhan@users.noreply.github.com>
2024-09-02 00:38:58 +0700

    Delete routes directory

--- routes/friendRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getFriendInfoByID, unFriendByID} = require("../controllers/userController");
-const router = express.Router();
-
-router.get('/:id', getFriendInfoByID);
-
-router.put('/unfriend/:id', unFriendByID)
-
-module.exports = router;
