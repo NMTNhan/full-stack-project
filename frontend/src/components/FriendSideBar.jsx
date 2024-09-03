36b98c88b6693070b044430699bc4439aeaad9eb
ThaiHoang <s3988412@rmit.edu.vn>
2024-09-03 22:50:11 +0700

    Update FriendSideBar.jsx

--- frontend/src/components/FriendSideBar.jsx
+++ frontend/src/components/FriendSideBar.jsx
@@ -3,7 +3,7 @@ import UserCard from "./UserCard";
 
 const FriendSidebar = ({friends}) => {
   return (
-      <div className="w-full max-w-md bg-gray-50 rounded-xl shadow-md py-8 px-8 mt-8 ml-8 mr-8">
+      <div className="w-full max-w-md bg-gray-50 rounded-xl shadow-md py-8 px-8 mr-8">
           <h2 className={'text-[28px] font-bold text-black mb-6 text-center'}>Friend</h2>
           <div className={'grid grid-cols-3 gap-2'}>
               {friends.map((friend) => (
