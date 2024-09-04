fb3b022e128047dcedb9943e22c1e2fb2546b186
Trong Nhan <92970779+NMTNhan@users.noreply.github.com>
2024-09-04 17:25:00 +0700

    updated Post and Admin routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const { suspendUser, approveGroup, deletePostByAdmin, deleteCommentByAdmin } = require('../controllers/adminController');
+const { getPendingGroups, resumeUser, suspendUser, approveGroup, deletePostByAdmin, deleteCommentByAdmin } = require('../controllers/adminController');
 const { protect } = require('../middleware/authMiddleware');
 const { isAdmin } = require('../middleware/adminMiddleware');
 
 const router = express.Router();
 
 // Define your admin routes here
+router.put('/users/:id/resume', protect, isAdmin, resumeUser);
+router.get('/groups', protect, isAdmin, getPendingGroups);
 router.put('/users/:id/suspend', protect, isAdmin, suspendUser);
 router.put('/groups/:id/approve', protect, isAdmin, approveGroup);
-router.delete('/posts/:id', protect, isAdmin, deletePostByAdmin);
+router.delete('/posts/:id', protect, isAdmin, deletePostByAdmin);  // Deleting posts
 router.delete('/posts/:postId/comments/:commentId', protect, isAdmin, deleteCommentByAdmin);
+
 
 module.exports = router;
